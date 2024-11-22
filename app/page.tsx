2195a69b33264deb79cc91c1ae615bcdd0ed5bd2
Rafay100 <srafay2021@gmail.com>
2024-11-22 04:22:49 -0800

    push my to_do list

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const Page = () => {
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
     if (title.trim() === "" || desc.trim() === "") {
-      alert("Both Title and Description are required.");
+      alert('Both Title and Description are required.');
       return;
     }
     setMainTask([...mainTask, { title, desc }]);
@@ -28,44 +28,44 @@ const Page = () => {
 
   return (
     <>
-      <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
-        Rafay's Todo List
+      <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>
+        Rafay Todo List
       </h1>
       <form onSubmit={submitHandler} className="flex flex-col items-center">
         <input
           type="text"
-          className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
-          placeholder="Enter Title Here"
+          className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2'
+          placeholder='Enter Title Here'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
-          type="text"
-          className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
-          placeholder="Enter Description Here"
+          type='text'
+          className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2'
+          placeholder='Enter Description Here'
           value={desc}
           onChange={(e) => setDesc(e.target.value)}
         />
-        <button className="bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5">
+        <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>
           Add Task
         </button>
       </form>
       <hr />
-      <div className="p-8 bg-slate-200">
+      <div className='p-8 bg-slate-200'>
         <ul>
           {mainTask.length > 0 ? (
             mainTask.map((task, index) => (
               <li
                 key={index}
-                className="flex items-center justify-between mb-8"
+                className='flex items-center justify-between mb-8'
               >
-                <div className="flex items-center justify-between mb-5 w-2/3">
-                  <h5 className="text-2xl font-semibold">{task.title}</h5>
-                  <h6 className="text-lg font-medium">{task.desc}</h6>
+                <div className='flex items-center justify-between mb-5 w-2/3'>
+                  <h5 className='text-2xl font-semibold'>{task.title}</h5>
+                  <h6 className='text-lg font-medium'>{task.desc}</h6>
                 </div>
                 <button
                   onClick={() => deleteHandler(index)}
-                  className="bg-red-400 text-white px-4 py-2 rounded font-bold"
+                  className='bg-red-400 text-white px-4 py-2 rounded font-bold'
                 >
                   Delete
                 </button>
